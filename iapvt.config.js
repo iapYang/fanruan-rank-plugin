5b9edf8b4827b706021462d9ec9f9823895f2d00
iapyang <yklorse@gmail.com>
2017-08-14 11:05:52 +0800

    u

--- iapvt.config.js
+++ iapvt.config.js
@@ -2,4 +2,8 @@ const path = require('path');
 
 module.exports = {
     template: path.resolve(__dirname, './dev/index.html'),
+    bundle: {
+        path: path.resolve('./dev/script/rank/index.js'),
+        name: 'rank',
+    },
 };
